3ddb84d1d5e32bf358017c01cfaefce19385891c
logicchains <jonathan.t.barnard@gmail.com>
2014-02-23 00:19:47 +0800

    More idiomatic F# implementation.

--- FS.fs
+++ FS.fs
@@ -1,45 +1,46 @@
-let NUM_RECORDS = 50 * 1000 * 444
+open System.Diagnostics
+
+let NUM_RECORDS = 50L * 1000L * 444L
 
 type FSMemTrade = { 
-  mutable TradeId : int64;
-  mutable ClientId : int64;
-  mutable VenueId : int32;
-  mutable InstrumentCode : int32;
-  mutable Price : int64;
-  mutable Quantity : int64;
-  mutable Side : char}
+  TradeId : int64;
+  ClientId : int64;
+  VenueId : int32;
+  InstrumentCode : int32;
+  Price : int64;
+  Quantity : int64;
+  Side : char}
 
-let trades : FSMemTrade array = Array.zeroCreate NUM_RECORDS
-let prepareArray = 
-    for i in 0 .. (NUM_RECORDS - 1) do
-      Array.set trades i {TradeId = (int64 0); ClientId = (int64 0); VenueId = (int32 0); InstrumentCode = (int32 0); Price = (int64 0); Quantity = (int64 0); Side = 'a'}
+let initTrades (tradesArray : FSMemTrade array) =
+    printfn "initiating trades"
+    for i in 0 .. tradesArray.Length - 1 do
+        let idx = int64 i
+        tradesArray.[i] <- {
+            TradeId = idx; 
+            ClientId = idx; 
+            VenueId = 123; 
+            InstrumentCode = 321;
+            Price = idx; 
+            Quantity = idx; 
+            Side = if (i % 2 = 0) then 'S' else 'B'
+        }
 
-let initTrades() =
-  printfn "initiating trades"
-  for i in 0 .. (NUM_RECORDS - 1) do
-    let trade = {
-      TradeId = (int64 i); 
-      ClientId = (int64 1); 
-      VenueId = (int32 123); 
-      InstrumentCode = (int32 321);
-      Price = (int64 i); 
-      Quantity = (int64 i); 
-      Side = if (i%2 = 0) then 'S' else 'B'}
-    Array.set trades i trade
-
-let perfRun n =
-  let startT = System.DateTime.Now.Ticks / System.TimeSpan.TicksPerMillisecond
-  let mutable buyCost = (int64 0)
-  let mutable sellCost = (int64 0)
-  initTrades()
-  for i in 0 .. (NUM_RECORDS - 1) do
-    if (trades.[i].Side = 'B') then buyCost <- trades.[i].Price * trades.[i].Quantity else sellCost <- trades.[i].Price * trades.[i].Quantity 
-  let endT = System.DateTime.Now.Ticks / System.TimeSpan.TicksPerMillisecond
-  let duration = endT - startT
+let perfRun n trades =
+  let sw = Stopwatch.StartNew()
+  let mutable buyCost = 0L
+  let mutable sellCost = 0L
+  initTrades trades
+  for trade in trades do
+    if (trade.Side = 'B') then 
+        buyCost <- trade.Price * trade.Quantity 
+    else 
+        sellCost <- trade.Price * trade.Quantity 
+  let duration = sw.ElapsedMilliseconds
+  sw.Stop()
   printfn "%d - duration %d ms" n duration
   printfn "buyCost = %O sellCost = %O" buyCost sellCost
 
 let main =
-  prepareArray
-  for i in 0 .. 4 do
-    perfRun i
+  let array = Array.zeroCreate (int32 NUM_RECORDS)
+  for i in 0 .. 5 do
+    perfRun i array
