46c21667232d1290107b361057daebdb1548a45e
Alex Miller <alex@puredanger.com>
2014-02-24 01:37:49 -0600

    tweak run params and aot the source

--- cjmt/project.clj
+++ cjmt/project.clj
@@ -5,5 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :main cjmt.core
-  :jvm-opts ["-Xmx1g" "-server"]
+  :aot :all
+  :jvm-opts ^:replace ["-server" "-Xmx2g"]
   :target-path "target/%s")
