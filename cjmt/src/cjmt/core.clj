232c2284acb8e4af624152f4f83becd19d4ab8ac
Alex Miller <alex@puredanger.com>
2014-02-24 01:38:24 -0600

    clean up and improve performance of clojure version

--- cjmt/src/cjmt/core.clj
+++ cjmt/src/cjmt/core.clj
@@ -7,56 +7,82 @@
 (def ^:const NUM_RECORDS (* 50 1000 444))
 
 (definterface IMemTest
-  (^Long gtradeId []) (^Long gclientId []) (^Long gvenueId []) (^Long ginstrumentCode []) (^Long gprice []) 
-  (^Long gquantity []) (^Character gside []) 
-  (stradeId [^Long v]) (sclientId [^Long v]) (svenueId [^Long v]) (sinstrumentCode [^Long v]) (sprice [^Long v])
-  (squantity [^Long v]) (sside [^Character v]))
+  (^long gtradeId [])
+  (^long gclientId [])
+  (^long gvenueId [])
+  (^long ginstrumentCode [])
+  (^long gprice [])
+  (^long gquantity [])
+  (^char gside [])
+  (stradeId [^long v])
+  (sclientId [^long v])
+  (svenueId [^long v])
+  (sinstrumentCode [^long v])
+  (sprice [^long v])
+  (squantity [^long v])
+  (sside [^char v]))
 
-(deftype CJMemTest [^:unsynchronized-mutable ^Long tradeId ^:unsynchronized-mutable ^Long clientId ^:unsynchronized-mutable ^Long venueId 
-                   ^:unsynchronized-mutable ^Long instrumentCode ^:unsynchronized-mutable ^Long price ^:unsynchronized-mutable ^Long quantity 
-                   ^:unsynchronized-mutable ^Character side]
+(deftype CJMemTest [^:unsynchronized-mutable ^long tradeId
+                    ^:unsynchronized-mutable ^long clientId
+                    ^:unsynchronized-mutable ^long venueId
+                    ^:unsynchronized-mutable ^long instrumentCode
+                    ^:unsynchronized-mutable ^long price
+                    ^:unsynchronized-mutable ^long quantity
+                    ^:unsynchronized-mutable ^char side]
   IMemTest
-  (gtradeId [_] tradeId)(gclientId [_] clientId)(gvenueId [_] venueId)(ginstrumentCode [_] instrumentCode)
-  (gprice [_] price)(gquantity [_] quantity)(gside [_] side)
-  (stradeId [this v] (set! tradeId v)) (sclientId [this v] (set! clientId v)) (svenueId [this v] (set! venueId v))
-  (sinstrumentCode [this v] (set! instrumentCode v))
-  (sprice [this v] (set! price v)) (squantity [this v] (set! quantity v))(sside [this v] (set! side v)))
+  (gtradeId [_] tradeId)
+  (gclientId [_] clientId)
+  (gvenueId [_] venueId)
+  (ginstrumentCode [_] instrumentCode)
+  (gprice [_] price)
+  (gquantity [_] quantity)
+  (gside [_] side)
+  (stradeId [this v] (set! tradeId v) nil)
+  (sclientId [this v] (set! clientId v) nil)
+  (svenueId [this v] (set! venueId v) nil)
+  (sinstrumentCode [this v] (set! instrumentCode v) nil)
+  (sprice [this v] (set! price v) nil)
+  (squantity [this v] (set! quantity v) nil)
+  (sside [this v] (set! side v) nil))
 
-(def trades ^"[Ljava.lang.Object;" (make-array Object NUM_RECORDS))
+(def trades ^objects (make-array IMemTest NUM_RECORDS))
 
 (defn init-trades []
-    (dotimes [i NUM_RECORDS]
-      (let [trade-ref ^CJMemTest (aget ^"[Ljava.lang.Object;" trades i)]
-        (do (.stradeId trade-ref i)
-               (.sclientId trade-ref 1)
-               (.svenueId trade-ref 123)
-               (.sinstrumentCode trade-ref 321)
-               (.sprice trade-ref i)
-               (.squantity trade-ref i)
-               (if (odd? i)
-                   (.sside trade-ref \S)
-                   (.sside trade-ref \B))))))
+  (let [trades ^objects trades]
+    (loop [i 0]
+      (when (< i NUM_RECORDS)
+        (doto ^CJMemTest (aget trades i)
+          (.stradeId i)
+          (.sclientId i)
+          (.svenueId 123)
+          (.sinstrumentCode 321)
+          (.sprice i)
+          (.squantity i)
+          (.sside (if (zero? (bit-and i 1)) \S \B)))
+        (recur (inc i))))))
 
-(defn perform-run [^Long run-num] 
-  (let  [start-t (System/currentTimeMillis)]
-    (do
-      (def buy-cost (long 0))
-      (def sell-cost (long 0))
-      (init-trades)
-      (dotimes [i NUM_RECORDS]
-        (let [trade-ref ^CJMemTest (aget ^"[Ljava.lang.Object;" trades i)]
-          (if (= (.gside trade-ref) \B)
-              (def buy-cost (+' buy-cost (* (.gprice trade-ref) (.gquantity trade-ref)))) 
-              (def sell-cost (+' sell-cost (* (.gprice trade-ref) (.gquantity trade-ref))))))) 
-      ;(printf "Run %d had duration %.6f seconds\n" run-num (- (System/currentTimeMillis) start-t) )
-      (printf "Run %d had duration " run-num)
-      (print (- (System/currentTimeMillis) start-t))
-      (println "ms")
-      (printf "buycost = %d sellCost = %d \n" (biginteger buy-cost) (biginteger sell-cost) ))))
+(defn perform-run [^long run-num]
+  (let  [start-t (System/currentTimeMillis)
+         trades ^objects trades
+         tlen (alength trades)]
+    (init-trades)
+    (loop [idx 0
+           buy-cost (bigint 0)
+           sell-cost (bigint 0)]
+      (if (< idx tlen)
+        (let [trade-ref ^CJMemTest (aget trades idx)]
+          (if (= \B (.gside trade-ref))
+            (recur (inc idx) (+ buy-cost (* (.gprice trade-ref) (.gquantity trade-ref))) sell-cost)
+            (recur (inc idx) buy-cost (+ sell-cost (* (.gprice trade-ref) (.gquantity trade-ref))))))
+        (do
+          (printf "Run %d had duration " run-num)
+          (print (- (System/currentTimeMillis) start-t))
+          (println "ms")
+          (printf "buycost = %d sellCost = %d \n" (biginteger buy-cost) (biginteger sell-cost)))))))
 
 (defn run []
-  (dotimes [i NUM_RECORDS] (aset ^"[Ljava.lang.Object;" trades i (CJMemTest. 1 1 1 1 1 1 \a)))
-  (dotimes [i 5] (perform-run i)))
+  (dotimes [i NUM_RECORDS] (aset ^objects trades i (CJMemTest. 1 1 1 1 1 1 \a)))
+  (dotimes [i 5] (System/gc) (perform-run i)))
 
 (defn -main []
   (run))
